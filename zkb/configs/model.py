28f558fe7046a029757fe4ac423d5e41b00e1d6e
Joseph <s3ppzer0@gmail.com>
2026-01-01 20:37:46 +0100

    fix: allow extra data to models

--- zkb/configs/model.py
+++ zkb/configs/model.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel, ConfigDict
 
 class ModelConfig(BaseModel):
     """Model configuration."""
-    model_config = ConfigDict(extra="allow")
+    model_config = ConfigDict(extra="ignore")
